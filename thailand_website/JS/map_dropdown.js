4235b16ca8c859a4c4119e066dd4374ee24dd8c1
akioda101 <49838873+akioda101@users.noreply.github.com>
2024-12-18 17:46:38 +0000

    added "view gallery" button on each destination page

--- thailand_website/JS/map_dropdown.js
+++ thailand_website/JS/map_dropdown.js
@@ -1,8 +1,22 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const toggleButton = document.querySelector(".dropdown-toggle");
-    const dropdownContent = document.querySelector(".dropdown-content");
+document.addEventListener("DOMContentLoaded", () => {
+    const mapButton = document.getElementById("toggle-map");
+    const galleryButton = document.getElementById("toggle-gallery");
+    const mapDropdown = document.getElementById("map-dropdown");
+    const galleryDropdown = document.getElementById("gallery-dropdown");
 
-    toggleButton.addEventListener("click", function () {
-        dropdownContent.classList.toggle("show");
+    mapButton.addEventListener("click", () => {
+        mapDropdown.classList.toggle("show");
+
+        if (galleryDropdown.classList.contains("show")) {
+            galleryDropdown.classList.remove("show");
+        }
+    });
+
+    galleryButton.addEventListener("click", () => {
+        galleryDropdown.classList.toggle("show");
+
+        if (mapDropdown.classList.contains("show")) {
+            mapDropdown.classList.remove("show");
+        }
     });
 });
